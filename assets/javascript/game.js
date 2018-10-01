10d6cd3f9e5ef42fe7fb78d63345510b322e58f4
richard <richard@guest-10-166-2-108.wireless.umn.edu>
2018-10-01 12:45:10 -0500

    <finnished up assignment a little late, because I fell behind in java>

---
+++ assets/javascript/game.js
@@ -0,0 +1,78 @@
+var letters = ["a", "b", "c","d","e","f","g","h","z"];
+
+
+var guessedLetters = [];
+
+
+var letterToGuess = null;
+
+
+var guessesLeft = 9;
+
+
+var wins = 0;
+var losses = 0;
+
+
+var updateGuessesLeft = function() {
+  document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+};
+
+var updateGuessesSoFar = function() {
+  document.querySelector("#guesses").innerHTML = guessedLetters.join(", ");
+};
+
+var reset = function() {
+  guessesLeft = 9;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+};
+
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+
+document.onkeydown = function(event) {
+ 
+  guessesLeft--;
+
+ 
+  var letter = String.fromCharCode(event.which).toLowerCase();
+
+ 
+  guessedLetters.push(letter);
+
+  
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+
+  
+  if (letter === letterToGuess) {
+
+    
+    wins++;
+    document.querySelector("#wins").innerHTML = wins;
+
+    
+    reset();
+  }
+
+  
+  if (guessesLeft === 0) {
+
+    
+    losses++;
+    document.querySelector("#losses").innerHTML = losses;
+
+    
+    reset();
+  }
+};
